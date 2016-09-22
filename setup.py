99af0a4fd77363128a746efe91d264ee78fb4060
André Driemeyer <andredriem@gmail.com>
2016-09-22 01:33:51 -0300

    setup and requirements prototypes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='KarmaTrain',
+    version='0.1',
+    packages=['tests', 'karmatrain'],
+    url='',
+    license='GPL-3.0',
+    author='André Driemeyer',
+    author_email='andredriem@gmail.com',
+    description='Reddit Analysis Tool'
+)
